33baa6052aab278511a61d31844d6f3226b65d88
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2025-07-29 19:25:40 +0100

    creating DOM elements

--- script.js
+++ script.js
@@ -61,6 +61,26 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements){
+
+  containerMovements.textContent = '';
+
+  movements.forEach(function(mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html =
+          `<div class="movements__row">
+            <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+            <div class="movements__value">${mov}</div>
+          </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
+displayMovements(account1.movements)
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -149,9 +169,7 @@ movements.forEach(function(mov, i, arr) {
 */
 
 // ----------------------------------------------------------152. forEach for Maps and Sets:
-
-
-
+/*
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
@@ -168,3 +186,8 @@ console.log(uniqueCurrencies);
 uniqueCurrencies.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 })
+*/
+
+// ----------------------------------------------------------153. Project Bankist app:
+
+// ----------------------------------------------------------154. creating DOM elements
