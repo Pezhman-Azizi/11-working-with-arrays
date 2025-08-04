02400a39a39558e28875ebc06132f6f068b68cd5
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2025-08-04 18:01:20 +0100

    17-flat-and-flatMap

--- script.js
+++ script.js
@@ -545,6 +545,7 @@ for (const acc of accounts){
 */
 // ----------------------------------------------------------165. implementing-log-in:
 // ----------------------------------------------------------169. Some and every
+/*
 // some=> if any of the elements meets the condition it returns true
 // every=> if all of the elements meet the condition it returns true
 console.log(movements.every(mov => mov > 0));
@@ -556,3 +557,36 @@ const deposit = mov => mov > 0
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+// ----------------------------------------------------------170. flat and flatMap method
+
+const arr = [[1,2,3], [4,5,6], 7, 8]
+console.log(arr.flat());
+
+
+const arrDeep = [[[1,2],3], [4,[5,6]], 7, 8]
+console.log(arrDeep.flat());
+// we can get pass the depth to the flat method
+console.log(arrDeep.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements)
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat()
+// console.log(allMovements);
+// const overallBalance = allMovements.reduce((acc, mov)=> acc+mov, 0)
+// console.log(overallBalance);
+
+// flat
+// const overallBalance = accounts
+// .map(acc => acc.movements)
+// .flat()
+// .reduce((acc, mov) => acc+ mov, 0)
+// console.log(overallBalance);
+
+// flatMap
+const overallBalance = accounts
+.flatMap(acc => acc.movements)
+.reduce((acc, mov) => acc+ mov, 0)
+console.log(overallBalance);
+// just so you know that flatMap goes one deeper alone not more!
